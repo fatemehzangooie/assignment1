b06ff80f178d8e5bd6bfa50a7d1a75d9ae2376ce
fatemehzangooie <110530784+fatemehzangooie@users.noreply.github.com>
2022-08-12 04:05:14 -0700

    Add files via upload

---
+++ adad.py
@@ -0,0 +1,5 @@
+adad=int(input('adad ra vared konid='))
+dovom=((adad // 10) % 10)
+panjom=((adad // 10000) % 10)
+sum=(dovom+panjom)
+print("majmue aadad=" , sum)
---
+++ mosalas.py
@@ -0,0 +1,4 @@
+b=float=(input('ghaedeh:'))
+h=float(input('ertefa:'))
+masahat=(b*h)/2
+print("masahat=" ,masahat)
